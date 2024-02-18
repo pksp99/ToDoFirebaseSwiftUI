fc8a8889b8ca438853dcb6653e4aae0df365d081
Preet Karia <karia.preet5@gmail.com>
2024-02-18 17:50:06 -0500

    Initial Commit

---
+++ ToDoFirebaseSwiftUI/ToDoFirebaseSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoFirebaseSwiftUIApp.swift
+//  ToDoFirebaseSwiftUI
+//
+//  Created by Preet Karia on 2/18/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoFirebaseSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
